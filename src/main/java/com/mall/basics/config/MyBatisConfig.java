53722f0f82fbcf01850a2034af3f63788fc5a49e
XuFei <1554084978@qq.com>
2021-02-04 17:41:12 +0800

    Initial commit

---
+++ src/main/java/com/mall/basics/config/MyBatisConfig.java
@@ -0,0 +1,13 @@
+package com.mall.basics.config;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * MyBatis配置类
+ * Created by macro on 2019/4/8.
+ */
+@Configuration
+@MapperScan({"com.mall.basics.mbg.mapper","com.mall.basics.dao"})
+public class MyBatisConfig {
+}
---
+++ src/test/java/com/mall/basics/BasicsApplicationTests.java
@@ -0,0 +1,14 @@
+package com.mall.basics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BasicsApplicationTests {
+
+    @Test
+    void contextLoads() {
+        System.out.println("test");
+    }
+
+}
